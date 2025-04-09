5db8b5b32f80b68cf323c4a6d21125cb1374fc36
AyushSN <ayushnimkar9@gmail.com>
2025-04-10 02:43:38 +0530

    fixed login

--- project/aries-ethereum-bridge/backend/src/controllers/user.controller.js
+++ project/aries-ethereum-bridge/backend/src/controllers/user.controller.js
@@ -1,30 +1,13 @@
 import * as userServices from '../services/user.service.js';
 
 const createUser = async (req, res) => {
-    const { username, password, email, context } = req.body;
-
-    if(context == "issuer") {
-    userServices.createIssuer(username, password, email, res);
-    }
-    else if(context == "holder") {
+    const { username, password, email } = req.body;
         userServices.createUser(username, password, email, res);
-    }
-    else {
-        res.status(400).json({ error: 'Invalid context' });
-    }
 };
 
 const loginUser = async (req, res) => {
     const { email, password } = req.body;
-    if(context == "issuer") {
-        userServices.loginIssuer(username, password, email, res);
-        }
-        else if(context == "holder") {
-            userServices.loginUser(username, password, email, res);
-        }
-        else {
-            res.status(400).json({ error: 'Invalid context' });
-        }
+            userServices.loginUser( email, password, res);
 };
 const testtoken = async (req, res) => {
     userServices.testtoken(res);
